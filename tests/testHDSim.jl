edac1dbacd22f42b1295a6ef23ace04a9ae9361a
Almog Yalinewich <almog.yalin@gmail.com>
2020-12-15 18:55:51 -0500

    add riemann problem test

--- tests/testHDSim.jl
+++ tests/testHDSim.jl
@@ -90,5 +90,49 @@ function runRiemannProblem2()
     return cond1 && cond2
 end
 
+function runRiemannProblem3()
+
+    # Initialise simulation
+    grid = range(0.0,1.0,length=100)
+    d_func = x->1
+    p_func = x->1
+    v_func = x->1.0*sign(x-0.5)
+    init_cond = Sirion.initHydroSnapshot(grid, d_func, p_func, v_func)
+    eos = Sirion.IdealGas(5.0/3.0)
+    tsf = Sirion.CFL(0.3)
+    vm = Sirion.Eulerian()
+    rs = Sirion.HLLC()
+    bc = Sirion.RigidWall(rs)
+    fc = Sirion.SimpleFluxCalculator(rs, bc)
+    pg = Sirion.Planar()
+    st = Sirion.ZeroSource()
+    cu = Sirion.SimpleCellUpdater()
+    sim = Sirion.initHDSim(init_cond,
+                          eos,
+                          tsf,
+                          vm,
+                          fc,
+                          pg,
+                          st,
+                          cu)
+
+    # Run simulation
+    while sim.time<0.22
+        Sirion.timeAdvance(sim)
+    end
+
+    # Perform diagnostics
+    pressure_probe = sim.state.cells[1+length(sim.state.cells)÷2].pressure
+    velocity_probe = sim.state.cells[1+length(sim.state.cells)÷2].velocity
+    reference_pressure = 0.224614
+    reference_velocity = 0.0
+
+    cond1 = calcRelativeDiff(reference_pressure, pressure_probe)<0.01
+    cond2 = abs(velocity_probe)<0.01
+
+    return cond1 && cond2
+end
+
 @test runRiemannProblem1()
 @test runRiemannProblem2()
+@test runRiemannProblem3()
