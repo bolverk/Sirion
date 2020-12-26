333e8e51b234ddd39f47ad6a237799b3ce32e19c
Almog Yalinewich <almog.yalin@gmail.com>
2020-12-26 09:04:05 -0500

    add sedov taylor test

--- tests/testHDSim.jl
+++ tests/testHDSim.jl
@@ -1,4 +1,9 @@
 using Test
+using Plots
+using Profile
+using FlameGraphs
+using Polynomials
+include("../src/Sirion.jl")
 
 function calcRelativeDiff(x::Float64, y::Float64)::Float64
     return abs(x-y)/(abs(x)+abs(y))
@@ -195,7 +200,130 @@ function runPlanarNoh()
 
 end
 
+function runSedovTaylor()
+
+    # Initialise simulation
+    #grid = range(1e-6,1.0,length=200)
+    grid = 10.0.^(range(-1.5, 0.0, length = 100))
+    #d_func = x->x<1e-2 ? 1e4 : x^-2
+    d_func = x->1
+    p_func = x->x<1e-1 ? 1e6 : 1e-9
+    v_func = x->0
+    init_cond = Sirion.initHydroSnapshot(grid, d_func, p_func, v_func)
+    g = 5.0/3.0
+    eos = Sirion.IdealGas(g)
+    tsf = Sirion.CFL(0.3)
+    vm = Sirion.Eulerian()
+    rs = Sirion.HLLC()
+    bc = Sirion.RigidWall(rs)
+    fc = Sirion.SimpleFluxCalculator(rs, bc)
+    pg = Sirion.Spherical()
+    #pg = Sirion.Planar()
+    st = Sirion.SphericalComplamentary()
+    #st = Sirion.ZeroSource()
+    cu = Sirion.SimpleCellUpdater()
+    sim = Sirion.initHDSim(init_cond,
+                          eos,
+                          tsf,
+                          vm,
+                          fc,
+                          pg,
+                          st,
+                          cu)
+
+    # Run simulation
+    #while sim.time<3e-3
+    time_list = Array{Float64}(undef,0)
+    radius_list = Array{Float64}(undef,0)
+    while sim.time<2e-3
+        Sirion.timeAdvance(sim)
+        mxval, mxind = findmax([c.velocity for c in sim.state.cells])
+        push!(time_list, sim.time)
+        push!(radius_list, sim.state.grid[mxind])
+    end
+
+    r_thres = 0.15
+    fitd = fit(map(log,time_list[radius_list .> r_thres]),
+               map(log,radius_list[radius_list .> r_thres]),
+               1)
+    #println(fitd)
+    #plot(time_list[radius_list .> r_thres],
+    #     radius_list[radius_list .> r_thres],
+    #     xaxis=:log,
+    #     yaxis=:log)
+    #plot!(time_list[radius_list .> r_thres],
+    #      [exp(fitd(log(x)))
+    #       for x in time_list[radius_list .> r_thres]],
+    #      xaxis=:log,
+    #      yaxis=:log)
+
+    return abs(fitd[1]-0.4)<2e-3
+end
+
+function runPrimakoff()
+
+    # Initialise simulation
+    #grid = range(1e-6,1.0,length=200)
+    grid = 10.0.^(range(-2.5, 0.0, length = 100))
+    d_func = x->x<1e-2 ? 1e4 : x^-2
+    #d_func = x->1
+    p_func = x->x<1e-2 ? 1e6 : 1e-9
+    v_func = x->0
+    init_cond = Sirion.initHydroSnapshot(grid, d_func, p_func, v_func)
+    g = 5.0/3.0
+    eos = Sirion.IdealGas(g)
+    tsf = Sirion.CFL(0.3)
+    vm = Sirion.Eulerian()
+    rs = Sirion.HLLC()
+    bc = Sirion.RigidWall(rs)
+    fc = Sirion.SimpleFluxCalculator(rs, bc)
+    pg = Sirion.Spherical()
+    #pg = Sirion.Planar()
+    st = Sirion.SphericalComplamentary()
+    #st = Sirion.ZeroSource()
+    cu = Sirion.SimpleCellUpdater()
+    sim = Sirion.initHDSim(init_cond,
+                          eos,
+                          tsf,
+                          vm,
+                          fc,
+                          pg,
+                          st,
+                          cu)
+
+    # Run simulation
+    #while sim.time<3e-3
+    while sim.time<3e-1
+        Sirion.timeAdvance(sim)
+        if sim.cycle%1000==0
+            println(sim.time)
+        end
+    end
+
+    # Perform diagnostics
+    p1 = plot(Sirion.midGrid(sim.state.grid),
+              [c.density for c in sim.state.cells],
+              legend=false)
+    p2 = plot(Sirion.midGrid(sim.state.grid),
+              [c.pressure for c in sim.state.cells],
+              legend=false)
+    p3 = plot(Sirion.midGrid(sim.state.grid),
+              [c.velocity for c in sim.state.cells],
+              legend=false)
+    p = plot(p1,p2,p3,layout=(3,1))
+    display(p)
+
+    #return false
+end
+
 @test runRiemannProblem1()
 @test runRiemannProblem2()
 @test runRiemannProblem3()
 @test runPlanarNoh()
+@test runSedovTaylor()
+#@profile runPrimakoff()
+#Juno.profiler()
+#Profile.clear()
+#open("./prof.txt", "w") do s
+#    Profile.print(IOContext(s, :displaysize => (24, 500)))
+#end
